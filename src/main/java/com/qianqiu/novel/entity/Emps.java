0383a2e3dc2eca63f01ae97812be46667becf9b6
jlkyl <jinglongkangyl@163.com>
2019-06-25 11:49:04 +0800

    loginB

--- src/main/java/com/qianqiu/novel/entity/Emps.java
+++ src/main/java/com/qianqiu/novel/entity/Emps.java
@@ -1,5 +1,12 @@
 package com.qianqiu.novel.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@Data
 public class Emps {
     private Integer empid;
     private String  uname;
@@ -7,121 +14,10 @@ public class Emps {
     private String empname;
     private String phone;
     private String idcard;
-    private String roleid;
-    private String state;
-    private String operateeid;
-    private String operatedate;
-
-    public Emps() {
-        super();
-    }
-
-    public Emps(Integer empid, String uname, String pwd, String empname, String phone, String idcard, String roleid, String state, String operateeid, String operatedate) {
-        this.empid = empid;
-        this.uname = uname;
-        this.pwd = pwd;
-        this.empname = empname;
-        this.phone = phone;
-        this.idcard = idcard;
-        this.roleid = roleid;
-        this.state = state;
-        this.operateeid = operateeid;
-        this.operatedate = operatedate;
-    }
-
-    public Integer getEmpid() {
-        return empid;
-    }
-
-    public void setEmpid(Integer empid) {
-        this.empid = empid;
-    }
-
-    public String getUname() {
-        return uname;
-    }
-
-    public void setUname(String uname) {
-        this.uname = uname;
-    }
-
-    public String getPwd() {
-        return pwd;
-    }
-
-    public void setPwd(String pwd) {
-        this.pwd = pwd;
-    }
-
-    public String getEmpname() {
-        return empname;
-    }
-
-    public void setEmpname(String empname) {
-        this.empname = empname;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getIdcard() {
-        return idcard;
-    }
-
-    public void setIdcard(String idcard) {
-        this.idcard = idcard;
-    }
-
-    public String getRoleid() {
-        return roleid;
-    }
-
-    public void setRoleid(String roleid) {
-        this.roleid = roleid;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getOperateeid() {
-        return operateeid;
-    }
-
-    public void setOperateeid(String operateeid) {
-        this.operateeid = operateeid;
-    }
-
-    public String getOperatedate() {
-        return operatedate;
-    }
-
-    public void setOperatedate(String operatedate) {
-        this.operatedate = operatedate;
-    }
-
-    @Override
-    public String toString() {
-        return "Emps{" +
-                "empid=" + empid +
-                ", uname='" + uname + '\'' +
-                ", pwd='" + pwd + '\'' +
-                ", empname='" + empname + '\'' +
-                ", phone='" + phone + '\'' +
-                ", idcard='" + idcard + '\'' +
-                ", roleid='" + roleid + '\'' +
-                ", state='" + state + '\'' +
-                ", operateeid='" + operateeid + '\'' +
-                ", operatedate='" + operatedate + '\'' +
-                '}';
-    }
+    private Integer roleid;
+    private Integer state;
+    private Integer operateeid;
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    private Date operatedate;
 }
