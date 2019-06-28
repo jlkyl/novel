e14932c261c1e7280c55a2a29716a0af09c57aa0
2771026718zx <2771026718@qq.com>
2019-06-28 10:45:02 +0800

    图书

--- src/main/java/com/qianqiu/novel/dao/IBooksDAO.java
+++ src/main/java/com/qianqiu/novel/dao/IBooksDAO.java
@@ -1,11 +1,34 @@
 package com.qianqiu.novel.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import com.qianqiu.novel.entity.Books;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface IBooksDAO {
 
+    @Select("SELECT b.*,bt.typename,u.username\n" +
+            "FROM books b LEFT JOIN booktype bt \n" +
+            "ON b.typeid = bt.typeid\n" +
+            "LEFT JOIN users u\n" +
+            "ON b.userid = u.userid\n" +
+            "ORDER BY bookid")
+    public List<Map<String,Object>> queryBook();
+
+    @Select("<script>" +
+            "SELECT b.*,bt.typename,u.pen\n" +
+            "FROM books b LEFT JOIN booktype bt \n" +
+            "ON b.typeid = bt.typeid\n" +
+            "LEFT JOIN users u\n" +
+            "ON b.userid = u.userid\n" +
+            "<where>" +
+            "<if test=\'kw!=null\'>or u.pen like concat('%',#{kw},'%')</if>" +
+            "<if test=\'kw!=null\'>or b.bookname like concat('%',#{kw},'%')</if>" +
+            "</where>" +
+            "</script>")
+    public List<Map<String,Object>> likeBook(@Param("kw") String kw);
 }
